d87a41cc4e53397643761053155a6ea19227a05f
Kromvel <31901087+Kromvel@users.noreply.github.com>
2021-02-18 17:03:37 +0300

    Add files via upload

---
+++ Modules/fragment_of_the_book.py
@@ -0,0 +1,20 @@
+# -*- coding: utf8 -*-
+import codecs
+import sys
+sys.path.append("..")
+import kirptestbot as b
+
+def fragment_of_book(message):
+    try:
+        if message.text == 'Бесы':
+            with codecs.open('Бесы.txt', encoding='utf-8') as demons:
+                read_demons = demons.read()
+            b.bot.send_message(message.chat.id, text=read_demons, reply_markup=b.mark_up_6)
+        elif message.text == 'Заповедник':
+            with codecs.open('Заповедник.txt', encoding='utf-8') as reserve:
+                read_reserve=reserve.read()
+            b.bot.send_message(message.chat.id, text=read_reserve, reply_markup=b.mark_up_6)
+        else:
+            b.bot.send_message(message.chat.id, 'Название книги неверное. Попробуйте снова', reply_markup=b.mark_up_6)
+    except:
+        b.bot.send_message(message.chat.id, 'Произошла ошибка. Попробуйте снова', reply_markup=b.mark_up_6)
